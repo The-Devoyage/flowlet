d8fbf4e46db29a97cb76644a3f6eac672201ee24
Nick McLean <nickisyourfan@gmail.com>
2025-06-25 21:37:26 -0500

    auth

--- lib/auth.py
+++ lib/auth.py
@@ -3,7 +3,13 @@ from lib.api import register, login
 from lib.utils import remove_var
 
 
-@click.command(name="register")
+@click.group(name="auth")
+def auth_group():
+    """Register and login to sync with cloud."""
+    pass
+
+
+@auth_group.command(name="register")
 @click.option("--email", prompt=True)
 @click.option("--password", prompt=True, hide_input=True, confirmation_prompt=True)
 def register_user(email, password):
@@ -11,18 +17,18 @@ def register_user(email, password):
     register(email, password)
 
 
-@click.command(name="login")
+@auth_group.command(name="login")
 @click.option("--email", prompt=True)
 @click.option("--password", prompt=True, hide_input=True)
 def login_user(email, password):
     """Login user on the remote server."""
     login(email, password)
 
-@click.command(name="logout")
+
+@auth_group.command(name="logout")
 def logout_user():
     """Sign the current user out."""
     # TODO: Implement Logout on Server
     # logout()
     remove_var("flowlet_token")
     click.secho("👋 Logged out successfully. See you soon!", fg="cyan", bold=True)
-
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import click
 
 from lib.command import commands_group
 from lib.vars import vars_group 
-from lib.auth_command import login_user, logout_user, register_user
+from lib.auth import auth_group
 
 DATA_FILE = Path.home() / ".flowlet.json"
 VARS_FILE = Path.home() / ".flowlet_vars.json"
@@ -17,9 +17,7 @@ def cli():
 
 cli.add_command(commands_group)
 cli.add_command(vars_group)
-cli.add_command(login_user)
-cli.add_command(register_user)
-cli.add_command(logout_user)
+cli.add_command(auth_group)
 
 if __name__ == "__main__":
     cli()
